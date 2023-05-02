61b1c92b096d6f3fa2dcf5f8f1c7dbbcba7fd15f
sekitaka <sekitaka.tech@gmail.com>
2023-05-02 10:22:38 +0900

    first commit

---
+++ Cloud/CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudApp.swift
+//  Cloud
+//
+//  Created by Takashi Seki on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
